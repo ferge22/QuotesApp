de54c92acfe866c562458fcf6ec800a7644d14ac
ferge22 <rytisspitrys@gmail.com>
2018-09-20 00:57:02 +0300

    setting structure

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
